f0a4736e4225fa5a58448be68277ea397a1ffc8d
ponos <187997037+ponos-diy@users.noreply.github.com>
2024-11-11 01:04:18 +0100

    fixup! hello world

--- generate_html.py
+++ generate_html.py
@@ -1,2 +1,6 @@
-with open("output/safety.html") as f:
+import os
+
+os.makedirs("output", exist_ok=True)
+
+with open("output/safety.html", "w") as f:
     f.write("<html><body><h1>hello world</h1></body></html>")
